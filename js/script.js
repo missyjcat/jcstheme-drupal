1973a30e5a16ed7264897731249d46962de69846
missyjcat <missyjcat@gmail.com>
2014-11-13 03:37:56 -0800

    stylig and js for scroll for portfolio sections of the site

--- js/script.js
+++ js/script.js
@@ -10,27 +10,50 @@
          * @param  {Number} testHeight - vert pos to trigger class
          * @param  {String} testEl - element to test for vert pos
          */
-        init: function(elClass, testClass, testHeight) {
+        init: function(elClass, testClass, testHeight, testEndHeight) {
+            if ($(elClass).length) {
 
-            var TESTHEIGHT = testHeight || ($(elClass).offset().top - 50);
+                var TESTHEIGHT = testHeight || ($(elClass).offset().top);
 
-            window.setInterval( function() {
+                window.setInterval( function() {
 
-                // Getting cross-browser compatible value of vertical
-                // position of scrollbar
-                vertPos = (window.pageYOffset !== undefined) ? window.pageYOffset : document.documentElement.scrollTop;
-                
-                if (vertPos > TESTHEIGHT && !($(elClass).hasClass(testClass))) {
-                    $(elClass).addClass(testClass);
-                } else if (vertPos < TESTHEIGHT && $(elClass).hasClass(testClass)) {
-                    $(elClass).removeClass(testClass);
-                }
-            }, 200);
+                    // Getting cross-browser compatible value of vertical
+                    // position of scrollbar
+                    vertPos = (window.pageYOffset !== undefined) ? window.pageYOffset : document.documentElement.scrollTop;
+                    
+                    if (vertPos > TESTHEIGHT && !($(elClass).hasClass(testClass))) {
+                        $(elClass).addClass(testClass);
+                    } else if (vertPos < TESTHEIGHT && $(elClass).hasClass(testClass)) {
+                        $(elClass).removeClass(testClass);
+                    }
+
+                    if (testEndHeight) {
+                        if (vertPos > testEndHeight) {
+                            $(elClass).removeClass(testClass);
+                        }
+                    }
+
+                }, 200);
+
+            }
         }
 
     };
 
+    // Exposing component for initialization
     jcsUtils.headerComponent = headerComponent;
+
+    /**
+     * Applying the "active" class in menu items for certain node types
+     */
+    
+    if ($('body').hasClass('node-type-portfolio-piece')) {
+        $('.portfolio-menu-item').addClass('active');
+    }
+
+    if ($('body').hasClass('node-type-article')) {
+        $('.blog-menu-item').addClass('active');
+    }
 
 })(window.jcsUtils = window.jcsUtils || {}, jQuery);
 
@@ -38,7 +61,18 @@
  * Initializing components
  */
 
+// Collapsing the main header
 jcsUtils.headerComponent.init('.header', 'header--collapsed', 100);
-jcsUtils.headerComponent.init('.aboutme__description__text', 'aboutme__description__text--collapsedheader', 100);
-jcsUtils.headerComponent.init('.aboutme__photo', 'aboutme__photo--collapsedheader', 100);
+
+// When header collapses, making sure the page adjusts to make up for the jump
+jcsUtils.headerComponent.init('.page', 'extrapadding', 100);
+
+// When the portfolio menu reaches the top, make it stick
 jcsUtils.headerComponent.init('.portfoliomenu', 'portfoliomenu--stickyheader');
+
+// These control the highlighting of the portfolio menu as the user scrolls on
+// the home page
+jcsUtils.headerComponent.init('.visualdesign-menu-item', 'color--orange', jQuery('.portfeed--visualdesign').offset().top - 200, jQuery('.portfeed--printdesign').offset().top - 200);
+jcsUtils.headerComponent.init('.printdesign-menu-item', 'color--reddish', jQuery('.portfeed--printdesign').offset().top - 200, jQuery('.portfeed--webdevelopment').offset().top - 200);
+jcsUtils.headerComponent.init('.webdevelopment-menu-item', 'color--lightgreen', jQuery('.portfeed--webdevelopment').offset().top - 200);
+
---
+++ templates/node--portfolio_piece.tpl.php
@@ -0,0 +1,67 @@
+<?php
+//kpr(get_defined_vars());
+//http://drupalcontrib.org/api/drupal/drupal--modules--node--node.tpl.php
+//node--[CONTENT TYPE].tpl.php
+
+//to remove all markup around a given field call the nomarkup theme function 
+//$content['field_name']['#theme'] = "nomarkup";
+
+if ($classes) {
+  $classes = ' class="'. $classes . ' "';
+}
+
+if ($id_node) {
+  $id_node = ' id="'. $id_node . '"';
+}
+
+$hideFieldsArray = [
+  'comments',
+  'links',
+  'field_live_link',
+  'body',
+  'field_image',
+  'field_portfolio_tags'
+];
+
+foreach ($hideFieldsArray as $field) {
+  hide($content[$field]);
+}
+
+ // print render($content['field_portfolio_tags']); 
+?>
+
+<?php if( theme_get_setting('mothership_poorthemers_helper') ){ ?>
+<!-- node.tpl.php -->
+<?php } ?>
+<article <?php print $id_node . $classes .  $attributes; ?> role="article">
+  <?php print $mothership_poorthemers_helper; ?>
+  <div class="content">
+    <div class="portdetail darkbg">
+      <div class="portdetail__container">
+        <?php print render($title_prefix); ?>
+        <h1 class="portdetail__title"><?php print $title ?></h1>
+        <?php print render($title_suffix); ?>
+        <div class="portdetail__tags"><?php print render($content['field_portfolio_tags']); ?></div>
+        <div class="portdetail__body"><?php print render($content['body']);?></div>
+        <div class="portdetail__livelink">
+          <?php
+            if (isset($content['field_live_link'][0])) { ?>
+              <span class="portdetail__livelink__label">live link:</span> <a class="portdetail__livelink__link" href="<?php print render($content['field_live_link'][0]['#markup']); ?>" target="_blank"><?php print render($content['field_live_link'][0]['#markup']); ?></a>
+          <?php } ?></div>
+        </div> <!-- /portdetail__container -->  
+    </div> <!-- /portdetail -->
+  
+    <div class="portimages">
+      <div class="portimages__container">
+        <?php print render($content['field_image']); ?>
+      </div>
+    </div>
+
+
+  </div> <!-- /content -->
+
+</article>
+
+<?php if( theme_get_setting('mothership_poorthemers_helper') ){ ?>
+<!-- /node.tpl.php -->
+<?php } ?>
