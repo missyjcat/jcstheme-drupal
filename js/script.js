9407138b967974446837e9249a6215891170ac3f
missyjcat <missyjcat@gmail.com>
2014-11-14 22:47:05 -0800

    added shadowbox and JS to make portfolio images show up

--- js/script.js
+++ js/script.js
@@ -3,39 +3,91 @@
     var headerComponent = {
 
         /**
-         * Initialize show/hide behavior for a header element of the site
-         * @param  {String} elClass - el to add class to
-         * @param  {String} testClass - classname to add when testHeight is 
+         * Given an el, class, and scroll height, add class to el when window
+         * reaches scroll height. Remove it when window is above scroll height.
+         * Optional testEndHeight which removes class after a certain height.
+         * 
+         * @param  {String} elQuery el to add class to - can be class or ID
+         * @param  {String} testClass classname to add when testHeight is 
          *          triggered
-         * @param  {Number} testHeight - vert pos to trigger class
-         * @param  {String} testEl - element to test for vert pos
+         * @param  {Number} testHeight vert pos to trigger class
+         * @param  {String} testEndHeight element to test for vert pos
          */
-        init: function(elClass, testClass, testHeight, testEndHeight) {
-            if ($(elClass).length) {
+        addClassOnScroll: function(elQuery, testClass, testHeight, testEndHeight) {
+            if ($(elQuery).length) {
 
-                var TESTHEIGHT = testHeight || ($(elClass).offset().top);
+                var TESTHEIGHT = testHeight || ($(elQuery).offset().top);
 
-                window.setInterval( function() {
+                var intervalId = setInterval( function() {
 
                     // Getting cross-browser compatible value of vertical
                     // position of scrollbar
                     vertPos = (window.pageYOffset !== undefined) ? 
                             window.pageYOffset : document.documentElement.scrollTop;
                     
-                    if (vertPos > TESTHEIGHT && !($(elClass).hasClass(testClass))) {
-                        $(elClass).addClass(testClass);
-                    } else if (vertPos < TESTHEIGHT && $(elClass).hasClass(testClass)) {
-                        $(elClass).removeClass(testClass);
+                    if (vertPos > TESTHEIGHT && !($(elQuery).hasClass(testClass))) {
+                        $(elQuery).addClass(testClass);
+                    } else if (vertPos < TESTHEIGHT && $(elQuery).hasClass(testClass)) {
+                        $(elQuery).removeClass(testClass);
                     }
 
                     if (testEndHeight) {
                         if (vertPos > testEndHeight) {
-                            $(elClass).removeClass(testClass);
+                            $(elQuery).removeClass(testClass);
                         }
                     }
 
                 }, 50);
 
+                return intervalId;
+
+            }
+        },
+
+        /**
+         * Given an el and some content, inject content into el when testHeight
+         * is scrolled to. Optional testEndHeight to remove content after.
+         * If `clearContent` is set to true, content will be cleared. If false
+         * then content will be restored to what it was before. The default is
+         * false.
+         * 
+         * @param  {String} elQuery - el to add class to - can be class or ID
+         * @param  {String} content classname to add when testHeight is 
+         *          triggered
+         * @param  {Number} testHeight vert pos to trigger class
+         * @param {Boolean} clearContent If true, content will be cleared. Default
+         *                               is false.
+         * @param  {String} testEndHeight element to test for vert pos
+         */
+        changeContentOnScroll: function(elQuery, content, testHeight, testEndHeight) {
+            if ($(elQuery).length) {
+
+                var TESTHEIGHT = testHeight || ($(elQuery).offset().top);
+                var TESTENDHEIGHT = testEndHeight || $(document).height();
+
+                var intervalId = setInterval( function() {
+
+                        // Getting cross-browser compatible value of vertical
+                        // position of scrollbar
+                        vertPos = (window.pageYOffset !== undefined) ? 
+                                window.pageYOffset : document.documentElement.scrollTop;
+                        
+                        if (vertPos > TESTHEIGHT && $(elQuery).html() !== content && vertPos < TESTENDHEIGHT) {
+                            $(elQuery).html(content);
+                        } 
+                        // else if (vertPos < TESTHEIGHT && $(elQuery).html() === content) {
+                        //     clearInterval(intervalId);
+                        // }
+
+                        // if (testEndHeight) {
+                        //     if (vertPos > testEndHeight) {
+                        //         clearInterval(intervalId);
+                        //     }
+                        // }
+
+                    }, 50);
+
+                return intervalId;
             }
         }
 
@@ -43,47 +95,149 @@
 
     $(document).ready( function() {
         /**
-         * Initializing components
+         * Storing procedural initialization of scroll behaviors but storing
+         * interval Ids in an array so we can clear them later when we call
+         * again upon window resize.
          */
+        
+        var activeSetIntervalIds = [];
+        
+        var initializeScroll = function() {
 
-        // Collapsing the main header
-        headerComponent.init('.header--collapsed', 'is-shown', 90);
+            intervalIdsArray = [];
 
-        // When header collapses, making sure the page adjusts to make up for the jump
-        // headerComponent.init('.page', 'extrapadding', 100);
+            // Collapsing the main header
+            intervalIdsArray.push(headerComponent.addClassOnScroll('.header--collapsed', 'is-shown', 90));
 
-        // These control the highlighting of the portfolio menu as the user scrolls on
-        // the home page
-        if ($('.portfoliomenu').length) {
+            // When header collapses, making sure the page adjusts to make up for the jump
+            // headerComponent.addClassOnScroll('.page', 'extrapadding', 100);
 
-            headerComponent.init(
-                    '.portfoliomenu--stickyheader',
-                    'is-shown',
-                    $('.portfoliomenu').offset().top - 39);
+            // These control the highlighting of the portfolio menu as the user scrolls on
+            // the home page
+            if ($('.portfoliomenu').length) {
 
-            headerComponent.init(
-                    '.visualdesign-menu-item', 
-                    'color--orange',
-                    $('.portfeed--visualdesign').offset().top - 200,
-                    $('.portfeed--printdesign').offset().top - 200);
+                intervalIdsArray.push(headerComponent.addClassOnScroll(
+                        '.portfoliomenu--stickyheader',
+                        'is-shown',
+                        $('.portfoliomenu').offset().top - 39));
 
-            headerComponent.init(
-                    '.printdesign-menu-item', 
-                    'color--reddish', 
-                    $('.portfeed--printdesign').offset().top - 200,
-                    $('.portfeed--webdevelopment').offset().top - 200);
+                intervalIdsArray.push(headerComponent.addClassOnScroll(
+                        '.design-menu-item', 
+                        'color--orange',
+                        $('.portfeed--design').offset().top - 200,
+                        $('.portfeed--otherprojects').offset().top - 200));
 
-            headerComponent.init(
-                    '.webdevelopment-menu-item',
-                    'color--lightgreen',
-                    $('.portfeed--webdevelopment').offset().top - 200);
+                intervalIdsArray.push(headerComponent.addClassOnScroll(
+                        '.otherprojects-menu-item', 
+                        'color--reddish', 
+                        $('.portfeed--otherprojects').offset().top - 200));
 
-            // Apply the active class on the homepage only if we hit the portfolio section
-            headerComponent.init(
-                    '.portfolio-menu-item',
-                    'active',
-                    $('.portfoliomenu').offset().top - 100);
-        }
+                intervalIdsArray.push(headerComponent.addClassOnScroll(
+                        '.webdevelopment-menu-item',
+                        'color--lightgreen',
+                        $('.portfeed--webdevelopment').offset().top - 200,
+                        $('.portfeed--design').offset().top - 200));
+
+                // Apply the active class on the homepage only if we hit the portfolio section
+                intervalIdsArray.push(headerComponent.addClassOnScroll(
+                        '.portfolio-menu-item',
+                        'active',
+                        $('.portfoliomenu').offset().top - 100));
+
+                return intervalIdsArray;
+
+            }
+        };
+
+        activeSetIntervalIds = activeSetIntervalIds.concat(initializeScroll());
+
+        /**
+         * If we're in mobile mode, tell the user what they're seeing in the
+         * portfolio as they are scrolling
+         */
+        
+        var initializeContentOnScroll = function() {
+
+            var intervalIdsArray = [];
+
+            if ($('.portfoliomenu').length) {
+                intervalIdsArray.push(headerComponent.changeContentOnScroll(
+                        '.portfolio-descriptor', 
+                        ' ',
+                        1,
+                        $('.portfeed--webdevelopment').offset().top - 200));
+
+                intervalIdsArray.push(headerComponent.changeContentOnScroll(
+                        '.portfolio-descriptor', 
+                        '<span class="color--orange">DESIGN</span>',
+                        $('.portfeed--design').offset().top - 200,
+                        $('.portfeed--otherprojects').offset().top - 200));
+
+                intervalIdsArray.push(headerComponent.changeContentOnScroll(
+                        '.portfolio-descriptor', 
+                        '<span class="color--reddish">OTHER</span>', 
+                        $('.portfeed--otherprojects').offset().top - 200));
+
+                intervalIdsArray.push(headerComponent.changeContentOnScroll(
+                        '.portfolio-descriptor',
+                        '<span class="color--lightgreen">WEBDEV</span>',
+                        $('.portfeed--webdevelopment').offset().top - 200,
+                        $('.portfeed--design').offset().top - 200));
+
+                return intervalIdsArray;
+            }
+
+        };
+
+        activeSetIntervalIds = activeSetIntervalIds.concat(initializeContentOnScroll());
+
+        /**
+         * Positioning the datelist and categories list below the menu titles
+         */
+        var doPositioning = function() {
+
+            if ($('.datelist').length) {
+                var elsArray  = [
+                    ['.datelist', '.blogheader__filters__dates'],
+                    ['.categorylist', '.blogheader__filters__categories']
+                ];
+
+                for (var i = 0; i < elsArray.length; i++) {
+                    var list = $(elsArray[i][0]);
+                    var menuPos = $(elsArray[i][1]).offset();
+
+                    list.css({
+                        top: menuPos.top + 41 + 'px',
+                        left: menuPos.left + 'px'
+                    });
+                }                
+            }
+
+            if ($(window).width() <= 945) {
+                $('.header--collapsed').addClass('js-mobile');
+            } else {
+                $('.header--collapsed').removeClass('js-mobile');
+            }
+
+        };
+
+        doPositioning();
+
+
+        $(window).resize( function() {
+            
+            // When window resizes, readjust the header scroll behavior
+            for (var i = 0; i < activeSetIntervalIds.length; i++) {
+                clearInterval(activeSetIntervalIds[i]);
+            }
+
+            activeSetIntervalIds.length = 0;
+            activeSetIntervalIds = activeSetIntervalIds.concat(initializeScroll());
+            activeSetIntervalIds = activeSetIntervalIds.concat(initializeContentOnScroll());
+
+            // When winow resizes, reposition the datelist and cateogrylist
+            doPositioning();
+        });
 
         /**
          * Removing the "active" class from portfolio when we hit the homepage
@@ -116,36 +270,6 @@
         $('.page-blog-categories .page-title, .page-blog-date .page-title').prependTo('.blogindex--wrapper');
 
         /**
-         * Positioning the datelist and categories list below the menu titles
-         */
-        var doPositioning = function() {
-
-            if ($('.datelist').length) {
-                var elsArray  = [
-                    ['.datelist', '.blogheader__filters__dates'],
-                    ['.categorylist', '.blogheader__filters__categories']
-                ];
-
-                for (var i = 0; i < elsArray.length; i++) {
-                    var list = $(elsArray[i][0]);
-                    var menuPos = $(elsArray[i][1]).offset();
-
-                    list.css({
-                        top: menuPos.top + 41 + 'px',
-                        left: menuPos.left + 'px'
-                    });
-                }                
-            }
-
-        };
-
-        doPositioning();
-
-        $(window).resize( function() {
-            doPositioning();
-        });
-
-        /**
          * Adding click listeners
          */
         
@@ -169,13 +293,12 @@
         });
 
         $('.carrot').click( function() {
-            $('.header--collapsed').addClass('js-mobile');
             $('.js-mobile ul').slideToggle('fast');
         });
 
-        $('.js-mobile li').click( function() {
+        $('.js-mobile ul li a').click( function() {
             $('.js-mobile ul').slideToggle('fast');
-        })
+        });
 
         /**
          * When user clicks on the portfolio menu item, scroll to the portfolio
@@ -186,6 +309,30 @@
                 $(window).scrollTop($('.portfoliomenu').offset().top - 38);
             }
         }
+
+        /**
+         * PORTFOLIO IMAGES
+         * When portfolio pages load, target each portfolio image and change
+         * the link source to the full res version of the image. Each thumbnail
+         * just has 'xxl' in the name, so we are just taking out that to link
+         * to the full image.
+         *
+         * Then also add rel="shadowbox" so that we can open the image with the
+         * shadowbox plugin.
+         *
+         * Maybe one day just do this in Drupal.
+         */
+        
+        $('.portimages__container a').each( function() {
+            var href = $(this).attr('href');
+            var regex = /xxlarge_/g;
+            href = href.replace(regex, '');
+            $(this).attr('href', href);
+            $(this).attr('rel', 'shadowbox[gallery]');
+        });
+
+        // Initiate shadowbox plugin
+        Shadowbox.init();
 
     });
 
